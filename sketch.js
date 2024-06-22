940c68ab5c50af4b7f63d870858ca58533ac7802
Sathish A <142662765+sathish-25-A@users.noreply.github.com>
2024-06-22 19:50:28 +0530

    Update sketch.js
    In this commit change, I inserted the variable scope to create an easy way to adjust the canvas width and height for different screen sizes.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
+var canvasWidth = 1525;
+var canvasHeight = 690;
 function setup() {
-  createCanvas(800, 300);
+  createCanvas(canvasWidth, canvasHeight);
   rectMode(CENTER);
 }
 
 function draw() {
   background(1, 186, 240);
+  var x = canvasWidth / 2;
+  var y = canvasHeight / 2;
   fill(237, 34, 93);
   noStroke();
-  ellipse(400, 150, 200, 200);
+  ellipse(x, y, 200, 200);
 
   fill(255);
-  rect(400, 150, 150, 30);
+  rect(x, y, 150, 30);
 }
